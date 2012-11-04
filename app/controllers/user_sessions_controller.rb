0f701bc23705a6deb912652ed405319c77a1e4eb
masaki_mac <fantastipo23@gmai.com>
2012-11-04 12:22:29 +0900

    implement auth logic

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -1,2 +1,31 @@
+# -*- coding: utf-8 -*-
 class UserSessionsController < ApplicationController
+  before_filter :require_user, :only => :destroy
+
+  def new
+   if current_user_session
+     current_user_session.destroy
+   end
+    @user_session = UserSession.new
+  end
+
+  # ログイン
+  def create
+    reset_session
+    @user_session = UserSession.new(params[:user_session])
+    if @user_session.save
+      flash[:notice] = "ログインしました。"
+      redirect_back_or_default user_url(current_user)
+    else
+      flash[:notice] = "ログインに失敗しました。"
+      render :action => :new
+    end
+  end
+
+  # ログアウト
+  def destroy
+    current_user_session.destroy
+    flash[:notice] = "ログアウトしました。"
+    redirect_back_or_default new_user_session_url
+  end
 end
---
+++ db/migrate/20121104020636_create_users.rb
@@ -0,0 +1,15 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :login
+      t.string :crypted_password
+      t.string :password_salt
+      t.string :persistence_token
+      t.integer :login_count
+      t.datetime :current_login_at
+      t.datetime :last_login_at
+
+      t.timestamps
+    end
+  end
+end
