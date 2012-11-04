fe1133921b26458163211ccfc8fdb1c4704d64a8
masaki_mac <fantastipo23@gmai.com>
2012-11-04 10:59:54 +0900

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CloudNoteServer::Application.config.secret_token = '1d777342663379dcf59dc57416b60d2e8c6920b770d0f959a6a4a02a18efdcfa9fb621baf47baad667fcde3d0fc9e81d8a884b72c774841bbcb464ed1ffda76e'
