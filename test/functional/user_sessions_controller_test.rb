8ef5ffb64645bee3c409890ddf3bf52089ddeb35
masaki_mac <fantastipo23@gmai.com>
2012-11-11 10:54:22 +0900

    add test for user_session controller

--- test/functional/user_sessions_controller_test.rb
+++ test/functional/user_sessions_controller_test.rb
@@ -1,7 +1,26 @@
 require 'test_helper'
 
 class UserSessionsControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+setup :activate_authlogic
+
+  test "should login" do
+    user = users(:general)
+    put :create, user_session: {login: user.login, password: 'benrocks'}
+    assert_redirected_to user_path(user)
+  end
+
+  test "should NOT login" do
+    user = users(:general)
+    put :create, user_session: {login: user.login, password: 'invalid_password'}
+    assert_template :new
+  end
+
+  test "should logout" do
+    user = users(:general)
+    assert UserSession.create(user)
+    delete :destroy, id: user.id
+    assert_redirected_to new_user_session_url
+    assert_nil session["user_credentials"]
+    assert_nil session["user_credentials_id"]
+  end
 end
